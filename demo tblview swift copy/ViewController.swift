f209036a6e07ff8b55e3f95c931c97a70d0b428b
Aftab khan <bsse20872@gmail.com>
2025-09-27 22:57:48 +0100

    Initial Commit

---
+++ demo tblview swift copy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo tblview swift copy
+//
+//  Created by aftab khan on 27/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
