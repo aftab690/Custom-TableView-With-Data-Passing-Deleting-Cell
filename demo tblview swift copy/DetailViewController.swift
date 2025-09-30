2b29d6b62741b78ba7109a02a0f083d6dd1511b5
Aftab khan <bsse20872@gmail.com>
2025-09-30 10:33:30 +0100

    fix github stage all changes

--- demo tblview swift copy/DetailViewController.swift
+++ demo tblview swift copy/DetailViewController.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var dimg: UIImageView!
+    
+    @IBOutlet weak var dlbl1: UILabel!
+    
+    @IBOutlet weak var dlbl2: UILabel!
+    
+    
+    var strlbl1:String!
+    var strlbl2:String!
+    var strimg:UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        dlbl1.text = strlbl1
+        dlbl2.text = strlbl2
+        dimg.image = strimg
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- demo tblview swift copy/ViewController.swift
+++ demo tblview swift copy/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tblview: UITableView!
+    var array1 = ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG", "HHH"]
+    var array2 = ["aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh"]
+    var arrimg = [#imageLiteral(resourceName: "c5"), #imageLiteral(resourceName: "c6"),#imageLiteral(resourceName: "c7"), #imageLiteral(resourceName: "c4"),#imageLiteral(resourceName: "c8"), #imageLiteral(resourceName: "c2"),#imageLiteral(resourceName: "c3"), #imageLiteral(resourceName: "c1")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        array2.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : TableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.lbl1.text = array1[indexPath.row]
+        cell.lbl2.text = array2[indexPath.row]
+        cell.img.image = arrimg[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // self.view.backgroundColor = UIColor.blue
+        let detail:DetailViewController = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        detail.strlbl1 = array1[indexPath.row]
+        detail.strlbl2 = array2[indexPath.row]
+        detail.strimg = arrimg[indexPath.row]
+        self.navigationController?.pushViewController(detail, animated: true)
+        
+        
+    
+    }
 
 }
 
