c220d40ab1d4cf9212cda7284e02e35edcd36c1f
Aftab khan <bsse20872@gmail.com>
2025-09-30 09:51:35 +0100

    data passing to detail screen

---
+++ demo tblview swift copy/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  demo tblview swift copy
+//
+//  Created by aftab khan on 29/09/2025.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
